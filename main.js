9413adcd93ee42d63e239ad7799e22c792fffd81
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-07 19:37:11 +0900

    feat: メンバーと部屋の一覧を表示

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-const test = () => {
-  console.log("Hello GAS!");
+function getMembers() {
+  // スプレッドシートのIDを指定
+  const spreadsheetId = '12PIQT57for32SwLgyU1Y8eYp2YGE_jPbk8zt8TewPRw'; // ここにスプレッドシートIDを入力
+  const sheet = SpreadsheetApp.openById(spreadsheetId).getActiveSheet();
+
+  // C列（3列目）のデータを取得（ヘッダーを除く）
+  const range = sheet.getRange(2, 3, sheet.getLastRow() - 1, 1);
+  const values = range.getValues();
+
+  // 配列として格納
+  const members = values.map(function(row) {
+    return row[0];
+  });
+
+  const rooms = ['room1', 'room2']
+
+  // 結果をログに出力
+  Logger.log(members);
+  Logger.log(rooms);
 }
