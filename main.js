dccdb35ed70f760e7505c978ac21167c6bb60168
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-07 22:07:05 +0900

    fix: 本番環境と揃えるための軽微な修正

--- main.js
+++ main.js
@@ -4,15 +4,11 @@ function membersShuffle(members) {
 }
 
 function getMembers() {
-  // スプレッドシートのIDを指定
-  const spreadsheetId = '12PIQT57for32SwLgyU1Y8eYp2YGE_jPbk8zt8TewPRw'; // ここにスプレッドシートIDを入力
+  const spreadsheetId = '12PIQT57for32SwLgyU1Y8eYp2YGE_jPbk8zt8TewPRw';
   const sheet = SpreadsheetApp.openById(spreadsheetId).getActiveSheet();
-
-  // C列（3列目）のデータを取得（ヘッダーを除く）
   const range = sheet.getRange(2, 3, sheet.getLastRow() - 1, 1);
   const values = range.getValues();
 
-  // 配列として格納
   const members = values.map(function(row) {
     return row[0];
   });
