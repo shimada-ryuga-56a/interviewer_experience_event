4bb19d9bc2be747550d51d4509f2da787a271ada
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-08 00:26:05 +0900

    improve: プライベート関数に変更

--- main.js
+++ main.js
@@ -1,36 +1,6 @@
-// メンバー取得
-function extractMembers(){
-  const spreadsheetId = '12PIQT57for32SwLgyU1Y8eYp2YGE_jPbk8zt8TewPRw';
-  const sheet = SpreadsheetApp.openById(spreadsheetId).getActiveSheet();
-  const range = sheet.getRange(2, 3, sheet.getLastRow() - 1, 1);
-  const values = range.getValues();
-  const members = values.map(function(row) {
-    return row[0];
-  });
-  return members;
-}
-
-// メンバーのシャッフル処理
-function membersShuffle(members) {
-  members.sort(() => Math.random() - 0.5);
-}
-
-// フォームの選択肢を更新
-function updatingFormChoices(question, choices) {
-  question.asMultipleChoiceItem().setChoiceValues(choices);
-}
-
-// 配列の各要素にindexを付す
-function addingIndexToArray(arr) {
-  result = arr.map(function(a, index) {
-    return (index + 1) + "：" + a
-  })
-  return result;
-}
-
 function roomDefinition() {
-  members = extractMembers();
-  membersShuffle(members);
+  members = extractMembers_();
+  membersShuffle_(members);
   const rooms = [];
 
   for (let i = 0; i < members.length; i += 2) {
@@ -48,12 +18,42 @@ function roomDefinition() {
 function setMemberOptions(){
   const feedbackFormId = '1HQInx4x88Za_tn2ghHVej9sB-pDovcQNF5Rpu3HBxzA';
   const feedbackForm = FormApp.openById(feedbackFormId);
-  members = extractMembers();
-  members = addingIndexToArray(members);
+  members = extractMembers_();
+  members = addingIndexToArray_(members);
 
   const items = feedbackForm.getItems();
   const question1 = items[0];
   const question2 = items[1];
-  updatingFormChoices(question1, members)
-  updatingFormChoices(question2, members)
+  updatingFormChoices_(question1, members)
+  updatingFormChoices_(question2, members)
+}
+
+// メンバー取得
+function extractMembers_(){
+  const spreadsheetId = '12PIQT57for32SwLgyU1Y8eYp2YGE_jPbk8zt8TewPRw';
+  const sheet = SpreadsheetApp.openById(spreadsheetId).getActiveSheet();
+  const range = sheet.getRange(2, 3, sheet.getLastRow() - 1, 1);
+  const values = range.getValues();
+  const members = values.map(function(row) {
+    return row[0];
+  });
+  return members;
+}
+
+// メンバーのシャッフル処理
+function membersShuffle_(members) {
+  members.sort(() => Math.random() - 0.5);
+}
+
+// フォームの選択肢を更新
+function updatingFormChoices_(question, choices) {
+  question.asMultipleChoiceItem().setChoiceValues(choices);
+}
+
+// 配列の各要素にindexを付す
+function addingIndexToArray_(arr) {
+  result = arr.map(function(a, index) {
+    return (index + 1) + "：" + a
+  })
+  return result;
 }
